f461339c081490270c064f4c7a6146c7a3cc6912
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2020-08-26 07:41:06 +0700

    Sine circle idk

--- run_game.py
+++ run_game.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import time
 
 pygame.font.init()
 
@@ -8,24 +9,46 @@ WINDOW = pygame.display.set_mode(WINDOW_SIZE)
 
 pygame.display.set_caption("#1")
 
-done = False
+running = True
 
-color = (0, 128, 255)
+deg1 = 0
+deg2 = 1
 
-is_blue = True
+CIRCLEWIDTH = 10
 
-while not done:
+autoSpin = True
+
+while running:
+    pygame.time.delay(8)
+    # clock.tick(60)
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_SPACE] and not autoSpin:
+        deg1 += 1
+        deg2 += 1
+
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            is_blue = not is_blue
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                autoSpin = not autoSpin
         if event.type == pygame.QUIT:
-            done = True
+            running = False
+            break
+    WINDOW.fill((255,255,255))
 
-    if is_blue:
-        color = (0, 128, 255)
-    else:
-        color = (255, 100, 0)
+    if autoSpin:
+        deg1 += 1
+        deg2 += 1
 
-    pygame.draw.rect(WINDOW, color, (30, 30, 60, 60))
+    sin = round(math.sin(math.radians(deg1)) * 100)
+    cos = round(math.cos(math.radians(deg2)) * 100)
 
-    pygame.display.flip()
+    centerY = round(WINDOW_SIZE[1] / 2 - CIRCLEWIDTH / 2)
+    centerX = int(round(WINDOW_SIZE[0] / 2 - CIRCLEWIDTH / 2))
+
+    pygame.draw.circle(WINDOW, (255,0,0), (centerX, sin + centerY), CIRCLEWIDTH)
+    pygame.draw.circle(WINDOW, (0,255,0), (cos + centerX, centerY), CIRCLEWIDTH)
+    # print(round(math.sin(math.radians(deg)) * 100))
+    pygame.display.update()
+
+pygame.exit()
